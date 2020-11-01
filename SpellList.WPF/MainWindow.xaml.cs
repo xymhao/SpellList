65a7bff5677db8d3a8594223515be3c2fc49f8b2
xuyueming <xnysym@outlook.com>
2020-11-01 14:57:01 +0800

    添加项目文件。

---
+++ SpellList.WPF/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SpellList.WPF.Annotations;
+
+namespace SpellList.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ProductDetail> DataList = new List<ProductDetail>();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            DataList = new List<ProductDetail>()
+            {
+                new ProductDetail("商品名称","商品价格"),
+                new ProductDetail("大蒜","2"),
+            };
+            ProductList.ItemsSource = DataList;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(NameText.Text);
+            DataList.Add(new ProductDetail(NameText, PriceText));
+            ProductList.UpdateLayout();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ProductList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class ProductDetail:INotifyPropertyChanged 
+    {
+        public ProductDetail(TextBox nameText, TextBox priceText)
+        {
+            Name = nameText.Text;
+            Price = priceText.Text;
+        }
+
+        public ProductDetail(string name, string price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Price { get; set ; }
+
+        public string Name { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SpellList/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System.Windows.Forms;
+
+namespace SpellList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param nameLable="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "",
+            ""}, -1);
+            this.nameLable = new System.Windows.Forms.Label();
+            this.priceLabe = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.商品名称 = new System.Windows.Forms.ColumnHeader();
+            this.上品价格 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // nameLable
+            // 
+            this.nameLable.AutoSize = true;
+            this.nameLable.Location = new System.Drawing.Point(109, 64);
+            this.nameLable.Name = "nameLable";
+            this.nameLable.Size = new System.Drawing.Size(56, 17);
+            this.nameLable.TabIndex = 0;
+            this.nameLable.Text = "商品名称";
+            this.nameLable.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // priceLabe
+            // 
+            this.priceLabe.AutoSize = true;
+            this.priceLabe.Location = new System.Drawing.Point(439, 67);
+            this.priceLabe.Name = "priceLabe";
+            this.priceLabe.Size = new System.Drawing.Size(56, 17);
+            this.priceLabe.TabIndex = 1;
+            this.priceLabe.Text = "商品价格";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(204, 64);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 23);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(552, 64);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 23);
+            this.priceTextBox.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(349, 106);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "添加";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.商品名称,
+            this.上品价格});
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(109, 198);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(602, 187);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.priceLabe);
+            this.Controls.Add(this.nameLable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLable;
+        private System.Windows.Forms.Label priceLabe;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListView listView1;
+        private ColumnHeader 商品名称;
+        private ColumnHeader 上品价格;
+    }
+}
+
